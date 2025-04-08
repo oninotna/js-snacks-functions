50dd685d55948cd64bd711b1970f4b825ecc4664
oninotna <antoninomusarratubbi@gmail.com>
2025-04-08 15:13:51 +0200

    02-iniziali ended

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,12 +1,35 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e 
+restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function searchAndSaveFirstChar(arrayToAnalize) {
+    const firstCharArray = [];
+    for (let i = 0; i < arrayToAnalize.length; i++) {
+        const currentElement = arrayToAnalize[i];
+        firstCharArray.push(currentElement[0]);
+    }
+
+    return firstCharArray;
+}
+
+const searchAndSaveFirstCharArrow = (arrayToAnalize) => {
+    const firstCharArray = [];
+    for (let i = 0; i < arrayToAnalize.length; i++) {
+        const currentElement = arrayToAnalize[i];
+        firstCharArray.push(currentElement[0]);
+    }
+
+    return firstCharArray;
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(searchAndSaveFirstChar(names));
+console.log(searchAndSaveFirstCharArrow(names));
 
 
 
